4ab3043f1f092777e50839fb08c66635997b6681
Hyungtae Moon <blessmht@gmail.com>
2018-06-04 16:25:13 +0900

    app/static폴더를 STATICFILES_DIRS에 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,6 +20,13 @@ print('BASE_DIR:', BASE_DIR)
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 print('TEMPLATES_DIR', TEMPLATES_DIR)
 
+STATIC_DIR = os.path.join(BASE_DIR,  'static')
+print('STATIC_DOR:', STATIC_DIR)
+
+# 정적파일을 탐색할 경로 목록
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
