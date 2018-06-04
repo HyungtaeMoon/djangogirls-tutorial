2bfa27d5e7ea4d7e569789d1183ae2790ac62b04
Hyungtae Moon <blessmht@gmail.com>
2018-06-04 12:47:00 +0900

    HTML파일을 불러올 때 render_to_string사용
    TEMPLATES_DIR을 지정 후, 해당 경로를 settings모듈의 TEMPLATES['DIR']에
    추가
    render_to_string에서 템플릿 파일을 찾기 위해 주어진 경로는
    settings모듈의 TEMPLATES['DIR']에 있는 폴더 목록을 기준으로 검색한다

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('BASE_DIR:', BASE_DIR)
+
+#os.path.join
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print('TEMPLATES_DIR', TEMPLATES_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +61,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
